715df7555b15b581437e464e3e476e746d60e3a1
Dash-02 <dasha.semivolos@gmail.com>
2024-04-22 23:01:56 +0300

    backend

--- preschool-landing/src/Auth/AuthParent.jsx
+++ preschool-landing/src/Auth/AuthParent.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import './AuthParent.css';
 import imgMom from '../assets/img/auth/mom.png';
 import imgDad from '../assets/img/auth/dad.png';
-
+import axios from "axios";
+import env from 'react-dotenv'
 function AuthParent() {
+    const [formData, setFormData] = useState({
+        fullName: '',
+        birthDate: '',
+        gender: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = () => {
+       
+        const AddParentInServer = async (formData) => {
+            try {
+                const {data} = await axios.post(`${env.BACKEND_URL}/auth_parent/register`, {
+                    fullName: formData.fullName,
+                    email: formData.email,
+                    gender: formData.gender,
+                    password: formData.password,
+                    dateBirth: formData.birthDate
+                })
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        AddParentInServer(formData)
+    };
 
     return (
         <section className="auth_parent">
@@ -14,12 +49,23 @@ function AuthParent() {
             <div className="auth_item">
                 <div className="input-item">
                     <span>ФИО родителя</span>
-                    <input type="text" placeholder="Иванов Иван Иванович" />
+                    <input 
+                        type="text" 
+                        name="fullName" 
+                        placeholder="Иванов Иван Иванович" 
+                        value={formData.fullName}
+                        onChange={handleChange}
+                    />
                 </div>
 
                 <div className="input-item">
                     <span>Дата рождения</span>
-                    <input type="date" />
+                    <input 
+                        type="date" 
+                        name="birthDate" 
+                        value={formData.birthDate}
+                        onChange={handleChange}
+                    />
                 </div>
 
                 <div className="input-item">
@@ -27,26 +73,49 @@ function AuthParent() {
                     <div className="wrapper_radio">
                         <label>
                             <span>мужской</span>
-                            <input className="radio" type="radio" name="gender" />
+                            <input 
+                                className="radio" 
+                                type="radio" 
+                                name="gender" 
+                                value="true"
+                                onChange={handleChange}
+                            />
                         </label>
                         <label>
                             <span>женский</span>
-                            <input className="radio" type="radio" name="gender" />
+                            <input 
+                                className="radio" 
+                                type="radio" 
+                                name="gender" 
+                                value="false"
+                                onChange={handleChange}
+                            />
                         </label>
                     </div>
                 </div>
 
                 <div className="input-item">
                     <span>Электронная почта</span>
-                    <input type="text" placeholder="example@email.com" />
+                    <input 
+                        type="text" 
+                        name="email" 
+                        placeholder="example@email.com" 
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
                 </div>
 
                 <div className="input-item">
                     <span>Пароль</span>
-                    <input type="password" />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
                 </div>
 
-                <button>ЗАРЕГИСТРИРОВАТЬСЯ</button>
+                <button onClick={handleSubmit}>ЗАРЕГИСТРИРОВАТЬСЯ</button>
             </div>
 
             <div className="auth_item">
